7d9b82e89fe047a8c484ff89fa844631bcb2a9c2
Bhavan Bhatt <bhavan009ld@gmail.com>
2023-02-16 16:06:11 +0530

    update

--- Basics-Of-Programming/Patterns/Lectures/Half-Pyramid.cpp
+++ Basics-Of-Programming/Patterns/Lectures/Half-Pyramid.cpp
@@ -2,8 +2,29 @@
 using namespace std;
 
 int main(){
-    // Numeric Half Pyramid
+    // Half Pyramid
     /*int rowcount,row,col;
+    cout << "Enter number of rows: ";
+    cin >> rowcount;
+    for(row=0;row<rowcount;row++){
+        for(col=0;col<row+1;col++){
+            cout << " * ";
+        }
+        cout << endl;
+    } */
+    // Inverted Half Pyramid
+    /*int rowcount,row,col;
+    cout << "Enter number of rows: ";
+    cin >> rowcount;
+    for(row=0;row<rowcount;row++){
+        for(col=0;col<rowcount - row;col++){
+            cout << " * ";
+        }
+        cout << endl;
+    }
+
+     // Numeric Half Pyramid
+    int rowcount,row,col;
     cout << "Enter number of rows: ";
     cin >> rowcount;
     for(row=0;row<rowcount;row++){
@@ -22,4 +43,5 @@ int main(){
         }
         cout << endl;
     }
+
 }
